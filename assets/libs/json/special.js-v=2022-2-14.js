90fa6d975069dc7cc38f3b6c03c414ddaa221d36
HyperLink Staff <105972903+HyperLink-Staff@users.noreply.github.com>
2023-01-19 16:17:25 +0800

    Upload try

---
+++ assets/libs/json/special.js-v=2022-2-14.js
@@ -0,0 +1,135 @@
+var special_data = {
+  "post_type": "special",
+  "posts": {
+    "movie": {
+      "1": {
+        "post_id": 1,
+        "title": "Disney Twisted-Wonderland KV and Theme Song Trailer",
+        "youtube": "GmgDkTFFNVs",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20KV%20and%20Theme%20Song%20Trailer&url=https://disneytwistedwonderland.com/special/?id=1",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=1",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=1"
+},
+      "2": {
+        "post_id": 2,
+        "title": "Disney Twisted-Wonderland Student Introduction Trailer",
+        "youtube": "2fTNWjw_heE",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20Student%20Introduction%20Trailer&url=https://disneytwistedwonderland.com/special/?id=2",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=2",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=2"
+      },
+      "3": {
+        "post_id": 3,
+        "title": "Disney Twisted-Wonderland Teaser Trailer - Heartslabyul Dorm",
+        "youtube": "t7_QsV7PcCs",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20Teaser%20Trailer%20-%20Heartslabyul%20Dorm&url=https://disneytwistedwonderland.com/special/?id=3",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=3",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=3"
+      },
+      "4": {
+        "post_id": 4,
+        "title": "Disney Twisted-Wonderland Teaser Trailer – Savanaclaw Dorm",
+        "youtube": "SOqR_YAob_c",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20Teaser%20Trailer%20%E2%80%93%20Savanaclaw%20Dorm&url=https://disneytwistedwonderland.com/special/?id=4",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=4",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=4"
+      },
+      "5": {
+        "post_id": 5,
+        "title": "Disney Twisted-Wonderland Teaser Trailer – Octavinelle Dorm",
+        "youtube": "pNcBk7F3bfQ",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20Teaser%20Trailer%20%E2%80%93%20Octavinelle%20Dorm&url=https://disneytwistedwonderland.com/special/?id=5",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=5",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=5"
+      },
+      "6": {
+        "post_id": 6,
+        "title": "Disney Twisted-Wonderland Teaser Trailer – Scarabia Dorm",
+        "youtube": "8bXOhdWmOck",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20Teaser%20Trailer%20%E2%80%93%20Scarabia%20Dorm&url=https://disneytwistedwonderland.com/special/?id=6",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=6",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=6"
+      },
+      "7": {
+        "post_id": 7,
+        "title": "Disney Twisted-Wonderland Teaser Trailer – Pomefiore Dorm",
+        "youtube": "BlSAH3iX-jc",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20Teaser%20Trailer%20%E2%80%93%20Pomefiore%20Dorm&url=https://disneytwistedwonderland.com/special/?id=7",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=7",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=7"
+      },
+      "8": {
+        "post_id": 8,
+        "title": "Disney Twisted-Wonderland Teaser Trailer – Ignihyde Dorm",
+        "youtube": "gXFjekDtMn8",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20Teaser%20Trailer%20%E2%80%93%20Ignihyde%20Dorm&url=https://disneytwistedwonderland.com/special/?id=8",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=8",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=8"
+      },
+      "9": {
+        "post_id": 9,
+        "title": "Disney Twisted-Wonderland Teaser Trailer – Diasomnia Dorm",
+        "youtube": "w5URvmFwlb4",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20Teaser%20Trailer%20%E2%80%93%20Diasomnia%20Dorm&url=https://disneytwistedwonderland.com/special/?id=9",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=9",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=9"
+      },
+      "10": {
+        "post_id": 10,
+        "title": "Disney Twisted-Wonderland - Seiichiro Yamashita and Chiaki Kobayashi Comment Video",
+        "youtube": "NpFSYR1RkaM",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20-%20Seiichiro%20Yamashita%20and%20Chiaki%20Kobayashi%20Comment%20Video&url=https://disneytwistedwonderland.com/special/?id=10",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=10",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=10"
+      },
+      "11": {
+        "post_id": 11,
+        "title": "Disney Twisted-Wonderland - Heartslabyul Animation Trailer 30s",
+        "youtube": "3Rxc9z_Pr_w",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20-%20Heartslabyul%20Animation%20Trailer%2030s&url=https://disneytwistedwonderland.com/special/?id=11",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=11",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=11"
+      },
+      "12": {
+        "post_id": 12,
+        "title": "Disney Twisted-Wonderland - Student Introduction Trailer",
+        "youtube": "2fTNWjw_heE",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20Student%20Introduction%20Trailer&url=https://disneytwistedwonderland.com/special/?id=12",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=12",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=12"
+      },
+      "13": {
+        "post_id": 13,
+        "title": "Disney Twisted-Wonderland - Savanaclaw Animation Trailer 30s",
+        "youtube": "tth-SMEmXxY",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20-%20Savanaclaw%20Animation%20Trailer%2030s&url=https://disneytwistedwonderland.com/special/?id=13",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=13",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=13"
+      },
+      "14": {
+        "post_id": 14,
+        "title": "Disney Twisted-Wonderland - Octavinelle Animation Trailer 30s",
+        "youtube": "56e-LyWx3ts",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20-%20Octavinelle%20Animation%20Trailer%2030s&url=https://disneytwistedwonderland.com/special/?id=14",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=14",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=14"
+      },
+      "15": {
+        "post_id": 15,
+        "title": "Disney Twisted-Wonderland - Scarabia Animation Trailer 30s",
+        "youtube": "cTlU1rtijio",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20-%20Scarabia%20Animation%20Trailer%2030s&url=https://disneytwistedwonderland.com/special/?id=15",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=15",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=15"
+      },
+       "16": {
+        "post_id": 16,
+        "title": "Disney Twisted-Wonderland - Pomefiore Animation  Trailer 30s",
+        "youtube": "GEQlKExIViI",
+        "share_tw": "http://twitter.com/share?text=Disney%20Twisted-Wonderland%20-%20Pomfiore%20Animation%20Trailer%2030s&url=https://disneytwistedwonderland.com/special/?id=16",
+        "share_fb": "https://www.facebook.com/sharer/sharer.php?u=https://disneytwistedwonderland.com/special/?id=16",
+        "share_cp": "\nhttps://disneytwistedwonderland.com/special/?id=16"
+      }
+    }
+  }
+}
